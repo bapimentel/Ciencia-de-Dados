4e25522317968babb6f7c5a0ef6490ef120cd34d
Bruno <bappimentel@gmail.com>
2019-10-02 18:12:34 -0300

    Atualizacao

--- preprocessamento.py
+++ preprocessamento.py
@@ -9,7 +9,6 @@ Created on Wed Oct  2 15:19:12 2019
 import numpy as np # used for handling numbers
 import pandas as pd # used for handling the dataset
 from sklearn.impute import SimpleImputer # used for handling missing data
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder # used for encoding categorical data
 from sklearn.model_selection import train_test_split # used for splitting training and testing data
 from sklearn.preprocessing import StandardScaler # used for feature scaling
 
@@ -17,3 +16,25 @@ dataset = pd.read_csv('Data.csv') # to import the dataset into a variable
 # Splitting the attributes into independent and dependent attributes
 X = dataset.iloc[:, :-1].values # attributes to determine dependent variable / Class
 Y = dataset.iloc[:, -1].values # dependent variable / Class
+
+# handling the missing data and replace missing values with nan from numpy and replace with mean of all the other values
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean') 
+imputer = imputer.fit(X[:, 1:])
+X[:, 1:] = imputer.transform(X[:, 1:])
+
+# encode categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder # used for encoding categorical data
+labelencoder_X = LabelEncoder()
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+onehotencoder = OneHotEncoder(categorical_features=[0])
+X = onehotencoder.fit_transform(X).toarray()
+labelencoder_Y = LabelEncoder()
+Y = labelencoder_Y.fit_transform(Y)
+
+# splitting the dataset into training set and test set
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
+
+# feature scaling
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
