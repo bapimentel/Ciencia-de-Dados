b650987c77ca4215a0c96a631be9d90f8ab97aac
Bruno <bappimentel@gmail.com>
2019-10-02 18:20:22 -0300

    Update preprocessamento.py

--- preprocessamento.py
+++ preprocessamento.py
@@ -5,25 +5,25 @@ Created on Wed Oct  2 15:19:12 2019
 @author: Bruno Pimentel
 """
 
-# libraries
-import numpy as np # used for handling numbers
-import pandas as pd # used for handling the dataset
-from sklearn.impute import SimpleImputer # used for handling missing data
-from sklearn.model_selection import train_test_split # used for splitting training and testing data
-from sklearn.preprocessing import StandardScaler # used for feature scaling
+# Bibliotecas
+import numpy as np
+import pandas as pd
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
-dataset = pd.read_csv('Dados/Data.csv') # to import the dataset into a variable
-# Splitting the attributes into independent and dependent attributes
-X = dataset.iloc[:, :-1].values # attributes to determine dependent variable / Class
-Y = dataset.iloc[:, -1].values # dependent variable / Class
+dataset = pd.read_csv('Dados/Data.csv') # importando os dados
+# Dividindo os dados em atributos dependentes e independentes
+X = dataset.iloc[:, :-1].values # atributos para determinar a classe
+Y = dataset.iloc[:, -1].values # variavel dependente
 
-# handling the missing data and replace missing values with nan from numpy and replace with mean of all the other values
+# tratando valores faltantes: substituindo os valores NaN pela media
 imputer = SimpleImputer(missing_values=np.nan, strategy='mean') 
 imputer = imputer.fit(X[:, 1:])
 X[:, 1:] = imputer.transform(X[:, 1:])
 
-# encode categorical data
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder # used for encoding categorical data
+# codificando dados categoricos
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X = LabelEncoder()
 X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
 onehotencoder = OneHotEncoder(categorical_features=[0])
@@ -31,10 +31,10 @@ X = onehotencoder.fit_transform(X).toarray()
 labelencoder_Y = LabelEncoder()
 Y = labelencoder_Y.fit_transform(Y)
 
-# splitting the dataset into training set and test set
+# dividindo os dados em treinamento e teste
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
 
-# feature scaling
+# normalizando as variaveis
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
